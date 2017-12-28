f5a329eb9d16c08c9db16fde3be776b53ccdc36b
Felix Köhnlein <felix.koehnlein@gmail.com>
2017-12-28 11:29:53 +0100

    Mortal reshaped to look good

--- car_park/Contract_Mortal.sol
+++ car_park/Contract_Mortal.sol
@@ -3,16 +3,22 @@ pragma solidity ^0.4.18;
 
 contract mortal
 {
- /* Define variable owner of the type address */
- address owner;
+	/* Define variable owner of the type address */
+	address owner;
 
 	/* This function is executed at initialization and sets the owner of the contract */
 	function mortal() public { owner = msg.sender; }
 
 	/* Function to recover the funds on the contract */
 	function kill() public
- {
-		if (msg.sender == owner) selfdestruct(owner);
-		else revert();
+	{
+		if (msg.sender == owner) 
+		{
+			selfdestruct(owner);
+		}
+		else
+		{
+			revert();
+		}
 	}
 }
